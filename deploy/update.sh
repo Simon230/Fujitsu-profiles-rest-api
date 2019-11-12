894b8df24a9ec4edd9d2b0bb66c5f53a20acd8de
simon230 <simonlerooy@yahoo.fr>
2019-11-12 11:57:05 +0100

    test pull error

--- deploy/update.sh
+++ deploy/update.sh
@@ -9,4 +9,4 @@ $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 supervisorctl restart profiles_api
 
-echo "DONE ! :)"
+echo "DONE !! :)"
